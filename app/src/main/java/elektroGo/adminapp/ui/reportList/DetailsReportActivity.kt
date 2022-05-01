97bcd5d52a05180e6ea0b795f6aabc5899c53c81
joel.cardona <joel.cardona@estudiantat.upc.edu>
2022-05-01 11:01:01 +0200

    Task#145_Finalitza la GUI, només falta la logica dels dos botons de report que es un altre tasca

--- app/src/main/java/elektroGo/adminapp/ui/reportList/DetailsReportActivity.kt
+++ app/src/main/java/elektroGo/adminapp/ui/reportList/DetailsReportActivity.kt
@@ -2,11 +2,45 @@ package elektroGo.adminapp.ui.reportList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import elektroGo.adminapp.R
 
 class DetailsReportActivity : AppCompatActivity() {
+    private lateinit var userWhoReports: TextView
+
+    private lateinit var reportedUser: TextView
+
+    private lateinit var reason: TextView
+
+    private lateinit var solveButton: Button
+
+    private lateinit var deleteUser: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_report)
+
+        userWhoReports = findViewById(R.id.Username)
+        reportedUser = findViewById(R.id.reportedUsername)
+        reason = findViewById(R.id.brieflyReason)
+        solveButton = findViewById(R.id.solveButton)
+        deleteUser = findViewById(R.id.deleteUserButton)
+
+        //Assignem els texts corresponents
+        userWhoReports.text = intent.getStringExtra("userWhoReports")
+        reportedUser.text = intent.getStringExtra("userReported")
+        reason.text = intent.getStringExtra("reason")
+
+        solveButton.setOnClickListener{
+            //TODO: FER LA CRIDA AMB BACKEND, CONTROL D'ERRORS I FER UN TOAST
+            finish()
+        }
+
+        deleteUser.setOnClickListener{
+            //TODO: FER LA CRIDA AMB BACKEND, CONTROL D'ERRORS I FER UN TOAST
+            finish()
+        }
+
     }
 }
