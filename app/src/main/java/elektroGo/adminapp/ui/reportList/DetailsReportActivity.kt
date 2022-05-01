7787233d004aa0e96da65c5fac538be8be2fb1a8
joel.cardona <joel.cardona@estudiantat.upc.edu>
2022-05-01 10:46:10 +0200

    Task#145_Afegeix els botons

--- app/src/main/java/elektroGo/adminapp/ui/reportList/DetailsReportActivity.kt
+++ app/src/main/java/elektroGo/adminapp/ui/reportList/DetailsReportActivity.kt
@@ -2,6 +2,7 @@ package elektroGo.adminapp.ui.reportList
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import elektroGo.adminapp.R
 
 class DetailsReportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
