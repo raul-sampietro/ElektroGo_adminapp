f93c48c62b9f656713092f6e09e34b4e3e34d3de
Joel Cardona <joelcardonsa@gmail.com>
2022-05-13 22:25:52 +0200

    Task#179_Crea la activity on es mostraran les dades completes del vehicle

---
+++ app/src/main/java/elektroGo/adminapp/ui/vehicleConfirmation/DetailsVehicleActivity.kt
@@ -0,0 +1,11 @@
+package elektroGo.adminapp.ui.vehicleConfirmation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsVehicleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_vehicle)
+    }
+}
