e0722db94db25fb79c392aae20150abfcbc2d93c
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-05-24 10:33:49 +0200

    Task#287_Refactor Controller

--- app/src/main/java/elektroGo/adminapp/controller/AdminAppController.kt
+++ app/src/main/java/elektroGo/adminapp/controller/AdminAppController.kt
@@ -1,22 +1,21 @@
 package elektroGo.adminapp.controller
 
-import android.app.ActionBar
 import elektroGo.adminapp.model.Reports
 import elektroGo.adminapp.model.Vehicle
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.android.*
-import io.ktor.client.features.*
-import io.ktor.client.features.get
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.http.*
 
 object AdminAppController {
-    private const val URL_BASE = "http://10.4.41.58:8080/"
+    //private const val URL_BASE_WB = "http://10.4.41.58:8080/"
+    //private const val URL_BASE = "http://10.4.41.58:8080";
+    private const val URL_BASE_WB = "http://192.168.1.82:8080/"
+    private const val URL_BASE = "http://192.168.1.82:8080"
 
     private val client = HttpClient(Android) {   //Exemple de com fer una crida amb el nostre servidor!
         expectSuccess = false
@@ -35,8 +34,26 @@ object AdminAppController {
         }
     }
 
+    // #################################################
+    // #  USERS                                        #
+    // #################################################
+
+    private const val URL_USERS = "${URL_BASE}/users"
+
+    suspend fun deleteUser(userToDelete: String): Int {
+
+        val httpResponse: HttpResponse = client.delete("${URL_USERS}/${userToDelete}")
+        return httpResponse.status.value
+    }
+
+    // #################################################
+    // #  REPORTS                                      #
+    // #################################################
+
+    private const val URL_REPORTS = "${URL_BASE}/reports"
+
     suspend fun getReportsList(): Pair<Int, ArrayList<Reports> >{
-        val reports: HttpResponse = client.get("${URL_BASE}users/Allreports")
+        val reports: HttpResponse = client.get(URL_REPORTS)
         val status: Int = reports.status.value
 
         val reportList: ArrayList<Reports>
@@ -46,25 +63,22 @@ object AdminAppController {
 
     }
 
-    //TODO add methods here
-    suspend fun deleteUser(userToDelete: String): Int {
-
-        val httpResponse: HttpResponse = client.post("${URL_BASE}users/delete") {
-            parameter("username", userToDelete)
-        }
-        return httpResponse.status.value
-    }
-
     suspend fun deleteReport(uWhoReports: String, uReported: String): Int {
-
-        val httpResponse: HttpResponse = client.post("${URL_BASE}user/unreport") {            parameter("userWhoReports", uWhoReports)
-            parameter("reportedUser", uReported)
-        }
+        val httpResponse: HttpResponse = client.delete("${URL_REPORTS}/from/${uWhoReports}/to/${uReported}")
         return httpResponse.status.value
     }
 
+    // #################################################
+    // #  VEHICLES                                     #
+    // #################################################
+
+    private const val URL_VEHICLES = "${URL_BASE}/vehicles"
+
+    //obté només els no verificats
     suspend fun getVehicleList(): Pair<Int, ArrayList<Vehicle>>{
-        val vehicles: HttpResponse = client.get("${URL_BASE}vehicles/notVerified")
+        val vehicles: HttpResponse = client.get(URL_VEHICLES) {
+            parameter("notVerified", true)
+        }
         val status: Int = vehicles.status.value
 
         val vehicleList: ArrayList<Vehicle>
@@ -74,16 +88,12 @@ object AdminAppController {
     }
 
     suspend fun acceptVehicle(numberPlate: String): Int {
-
-        val httpResponse: HttpResponse = client.put("${URL_BASE}vehicles/verify/${numberPlate}")
+        val httpResponse: HttpResponse = client.put("${URL_VEHICLES}/${numberPlate}/verify")
         return httpResponse.status.value
     }
 
     suspend fun rejectVehicle(numberPlate: String): Int {
-
-        val httpResponse: HttpResponse = client.delete("${URL_BASE}vehicles/${numberPlate}")
+        val httpResponse: HttpResponse = client.delete("${URL_VEHICLES}/${numberPlate}")
         return httpResponse.status.value
     }
-
-
 }
